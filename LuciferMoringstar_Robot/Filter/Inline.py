ab92845272272fba6490089c172f54e5f1b8d194
CYBERIBLEES <88227420+CYBERIBLEES@users.noreply.github.com>
2021-10-01 17:26:59 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -93,10 +93,7 @@ def get_reply_markup(query):
         ],
         [
         InlineKeyboardButton('↩️ SEARCH AGAIN ↪️', switch_inline_query_current_chat=query)
-        ],
-        [InlineKeyboardButton('↩️ SEARCH NEW ↪️', switch_inline_query_current_chat=)
-        ]]
-
+        ]
     return InlineKeyboardMarkup(buttons)
 
 
